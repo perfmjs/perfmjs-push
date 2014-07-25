70338cefcd922f252d9b8da8cfc210257491df12
perfmjs <19589425@qq,cin>
2014-07-25 21:54:28 +0800

    add forever mark for start-server-cluster2.js

--- start-server-cluster2.js
+++ start-server-cluster2.js
@@ -1,6 +1,6 @@
 /**
  * 应用入口函数
- * 后台启动 /usr/local/node/bin/forever start -a -l /www/perfmjs-push-1.0.8/logs/forever.log -o logs/out.log -e logs/err.log start-server-cluster.js; tail -f ./logs/out.log
+ * 后台启动 /usr/local/node/bin/forever start -a -l /www/perfmjs-push-1.0.8/logs/forever2.log -o logs/out2.log -e logs/err2.log start-server-cluster2.js; tail -f ./logs/out2.log
  */
 require("perfmjs-node");
 perfmjs.ready(function($$, app) {
