7b7729d7768da1b92e334f61c2ba11d739409891
perfmjs <19589425@qq,cin>
2014-08-14 16:58:44 +0800

    增加AMD模块化实现

--- start-server-cluster.js
+++ start-server-cluster.js
@@ -1,6 +1,6 @@
 /**
  * 应用入口函数
- * 后台启动 /usr/local/node/bin/forever start -a -l /www/perfmjs-push-1.0.9/logs/forever.log -o logs/out.log -e logs/err.log start-server-cluster.js; tail -f ./logs/out.log
+ * 后台启动 /usr/local/node/bin/forever start -a -l /www/perfmjs-push-1.1.0/logs/forever.log -o logs/out.log -e logs/err.log start-server-cluster.js; tail -f ./logs/out.log
  */
 require("perfmjs-node");
 perfmjs.ready(function($$, app) {
--- start-server-cluster2.js
+++ start-server-cluster2.js
@@ -1,6 +1,6 @@
 /**
  * 应用入口函数
- * 后台启动 /usr/local/node/bin/forever start -a -l /www/perfmjs-push-1.0.9/logs/forever2.log -o logs/out2.log -e logs/err2.log start-server-cluster2.js; tail -f ./logs/out2.log
+ * 后台启动 /usr/local/node/bin/forever start -a -l /www/perfmjs-push-1.1.0/logs/forever2.log -o logs/out2.log -e logs/err2.log start-server-cluster2.js; tail -f ./logs/out2.log
  */
 require("perfmjs-node");
 perfmjs.ready(function($$, app) {
